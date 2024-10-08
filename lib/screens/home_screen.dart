d87808621cc508bba1dd9d9b4775577bf43d13f4
Saiyam Mittal <sammittal1608@gmail.com>
2024-10-08 16:30:40 +0530

    add widgets and improve the UI

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,6 +1,15 @@
 import 'package:flutter/material.dart';
-import '../widgets/market_statistics.dart'; // Import Market Statistics widget
-import '../widgets/OptionsStrategyCard.dart'; // Import Options Strategy Card widget
+import '../widgets/market_statistics.dart';
+import '../widgets/OptionStrategy/option_strategy_slider.dart';
+import '../widgets/bottom_navigation/custom_bottom_navigation_bar.dart';
+import '../widgets/portfolio_overview_card.dart';
+import '../widgets/investment_products_grid.dart';
+import '../widgets/predict_trade_options_card.dart';
+import '../widgets/Trending_Stock_Baskets_Card.dart';
+import '../widgets/invest_in_ipo_card.dart';
+import '../widgets/research_idea_section/section.dart';
+import '../widgets/subscription/subscription_offer.dart';
+import '../widgets/subscription/subscription_content.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -10,9 +19,9 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-  int _selectedIndex = 0; // Index to manage the selected tab in BottomNavigationBar
+  int _selectedIndex = 0;
+  bool isSubscribed = true;
 
-  // Function to handle tap on bottom navigation bar
   void _onItemTapped(int index) {
     setState(() {
       _selectedIndex = index;
@@ -23,15 +32,26 @@ class _HomeScreenState extends State<HomeScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: _buildAppBar(),
+      backgroundColor: const Color(0xFFF5F5F5),
       body: ListView(
-        padding: const EdgeInsets.fromLTRB(10, 2, 8, 2),
+        padding: const EdgeInsets.symmetric(horizontal: 14, vertical: 2),
         children: [
-          const SizedBox(height: 20),
-          const MarketStatistics(), // Add MarketStatistics widget here
-          const OptionsStrategyCard(), // Add OptionsStrategyCard widget here
+          const SizedBox(height: 2),
+          const MarketStatistics(),
+          const OptionsStrategySlider(),
+          const PortfolioOverviewCard(),
+          const InvestmentProductsGrid(),
+          const PredictTradeOptionsCard(),
+          const TrendingStockBasketsSection(),
+          const InvestInIPOCard(),
+          const ResearchIdeaSection(),
+          // isSubscribed ? const SubscriptionContent() : const SubscriptionOffer(),
         ],
       ),
-      bottomNavigationBar: _buildBottomNavigationBar(),
+      bottomNavigationBar: CustomBottomNavigationBar(
+        selectedIndex: _selectedIndex,
+        onTap: _onItemTapped,
+      ),
     );
   }
 
@@ -81,39 +101,21 @@ class _HomeScreenState extends State<HomeScreen> {
           hintStyle: TextStyle(color: Colors.grey[600]),
           suffixIcon: Icon(Icons.search, color: Colors.grey),
           border: InputBorder.none,
-          contentPadding: EdgeInsets.symmetric(vertical: 8, horizontal: 8),
+          contentPadding: const EdgeInsets.symmetric(vertical: 8, horizontal: 8),
         ),
       ),
     );
   }
 
   Widget _buildIconButton(String assetPath, VoidCallback onPressed) {
-    return Expanded(
-      flex: 1,
-      child: Align(
-        alignment: Alignment.centerLeft,
-        child: IconButton(
-          icon: Image.asset(assetPath, width: 20, height: 20),
-          padding: EdgeInsets.zero,
-          constraints: BoxConstraints(),
-          onPressed: onPressed,
-        ),
+    return Align(
+      alignment: Alignment.centerLeft,
+      child: IconButton(
+        icon: Image.asset(assetPath, width: 20, height: 20),
+        padding: EdgeInsets.zero,
+        constraints: const BoxConstraints(),
+        onPressed: onPressed,
       ),
-    );
-  }
-
-  BottomNavigationBar _buildBottomNavigationBar() {
-    return BottomNavigationBar(
-      items: const <BottomNavigationBarItem>[
-        BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
-        BottomNavigationBarItem(icon: Icon(Icons.view_list), label: 'Watchlist'),
-        BottomNavigationBarItem(icon: Icon(Icons.explore), label: 'Explore'),
-        BottomNavigationBarItem(icon: Icon(Icons.pie_chart), label: 'Portfolio'),
-        BottomNavigationBarItem(icon: Icon(Icons.show_chart), label: 'Trades'),
-      ],
-      currentIndex: _selectedIndex,
-      selectedItemColor: Colors.blueAccent,
-      onTap: _onItemTapped,
     );
   }
 }
